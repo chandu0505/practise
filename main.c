e48a25cb113f3e58d5b5587c7b82744bfb68f00c
chandu <msbc0505@gmail.com>
2024-06-14 18:49:48 +0530

    Remove the second number from the list

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int numbers[4] = {1, 2, 3, 4};
-    int count = 4;
-
-    for (int i = 0; i < count; i++) {
-        numbers[i] *= 2;
-    }
+    int numbers[2] = {1, 3}; // Removed the second number
+    int count = 2;
 
     for (int i = 0; i < count; i++) {
         printf("%d ", numbers[i]);
